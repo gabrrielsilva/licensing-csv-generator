5f6a76b70d35b78d30a9cfe3509e611c6b1a717c
gabrrielsilva <gabrielsilvaev@gmail.com>
2024-06-18 22:06:10 -0300

    fist commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import fs from 'node:fs';
+
+const attOut = fs.readFileSync('assets/BLOCO POSTE PROCISA.txt', 'utf-8');
+const attOutRows = attOut.trim().split('\n');
+
+// Remove header
+attOutRows.shift();
+
+const csvHeader = "ID,Extensão da rede(m),Coordenada X,Coordenada Y,MT(SIM/NÃO),AT(SIM/NÃO),Intervenção(INST/ESPI),Equipamento,Altura poste(m),Esforço(kgf),Instalação,Protocolo de aprovação do ponto,Diâmetro cabo (mm),Tipo de cabo,Diâmetro de apoio(mm),Logradouro,Bairro,Observações";
+const csvRows = [csvHeader];
+
+// Process each line of file
+attOutRows.map(row => {
+  const columns = row.replace('\r', '').split('\t');
+
+  const poleId = columns[4];
+  const poleHeight = columns[2].split('/')[0];
+  const poleKgf = columns[5];
+  const poleExistingNetwork = columns[6];
+  const poleCoordinateX = columns[7].replace('X=', '');
+  const poleCoordinateY = columns[8].replace('Y=', '');;
+  const poleIntervention = poleExistingNetwork === 'SIM' ? 'ESPI' : 'INST';
+  const poleInstallation = poleExistingNetwork === 'SIM' ? 'EXIS' : 'NOVA';
+
+  csvRows.push(`${poleId},,${poleCoordinateX},${poleCoordinateY},SIM,NÃO,${poleIntervention},NOEQ,${poleHeight},${poleKgf},${poleInstallation},,20,FIBR,4.8`);
+});
+
+const byteOrderMark = '\uFEFF';
+const csvContent = byteOrderMark + csvRows.join('\n');
+fs.writeFileSync('output.csv', csvContent, 'utf-8');
